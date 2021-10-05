17aea0917122792ac9ef684a6318b80a68a2d530
Piotr Drabik <piotr.drabik@edu.uni.lodz.pl>
2021-10-05 08:54:02 +0200

    bad plane error

--- sample_algorithm/sample.cpp
+++ sample_algorithm/sample.cpp
@@ -6,9 +6,17 @@
 Sample::Sample(const Plane &other) : width_(other.GetWidth()), height_(other.GetHeight()) {
   copy_plane_.reserve(other.GetHeight() * other.GetWidth());
 
+  bool is_there_starting_point = false;
+  bool is_there_destination_point = false;
+
   for (int y = 0; y < other.GetHeight(); y++)
-    for (int x = 0; x < other.GetWidth(); x++)
+    for (int x = 0; x < other.GetWidth(); x++) {
       copy_plane_.emplace_back(other.GetCell({x, y}));
+      if (other.GetCell({x, y}) == CellType::START) is_there_starting_point = true;
+      if (other.GetCell({x, y}) == CellType::FINISH) is_there_destination_point = true;
+    }
+
+  if (not is_there_starting_point or not is_there_destination_point) throw "bad plane error";
 }
 
 Sample::Sample(const Sample &other) : width_(other.width_), height_(other.height_) {
--- sample_algorithm/sample.h
+++ sample_algorithm/sample.h
@@ -14,6 +14,7 @@ class Sample {
 
 
 
+
   protected:
    /// x axis
    unsigned width_;
