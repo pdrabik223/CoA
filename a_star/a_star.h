cf18d8b4cdf52c91dc7eebd317cc0941d40dec8e
Piotr Drabik <piotr.drabik@edu.uni.lodz.pl>
2021-10-06 21:19:03 +0200

    new a star directory

---
+++ a_star/a_star.h
@@ -0,0 +1,13 @@
+//
+// Created by piotr on 06/10/2021.
+//
+
+#ifndef COA__A_STAR_H_
+#define COA__A_STAR_H_
+namespace a_star {
+class AStar {
+
+
+};
+}
+#endif//COA__A_STAR_H_
---
+++ a_star/cell.h
@@ -0,0 +1,11 @@
+//
+// Created by piotr on 06/10/2021.
+//
+
+#ifndef COA_A_STAR_CELL_H_
+#define COA_A_STAR_CELL_H_
+namespace a_star {
+struct Cell {
+  };
+}
+#endif//COA_A_STAR_CELL_H_
