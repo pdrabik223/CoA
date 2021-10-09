daf141b74ca712e05db94b3bab367fe652863e17
Piotr Drabik <piotr.drabik@edu.uni.lodz.pl>
2021-10-09 07:49:53 +0200

    random walk is displaying stuff, but has it's problems

---
+++ random_walk/random_walk_algorithm.h
@@ -0,0 +1,70 @@
+//
+// Created by piotr on 05/10/2021.
+//
+
+#ifndef COA_RANDOM_WALK_RANDOW_WALK_ALGORITHM_H_
+#define COA_RANDOM_WALK_RANDOW_WALK_ALGORITHM_H_
+#include "../plane/plane.h"
+#include "../sample_algorithm/cell.h"
+#include "../sfml_window/window.h"
+#include "../sfml_window/window_plane.h"
+
+class RandomWalkAlgorithm {
+
+ public:
+  RandomWalkAlgorithm() = delete;
+  /// constructs Sample object based of Plane
+  RandomWalkAlgorithm(const Plane &other);
+  RandomWalkAlgorithm(const RandomWalkAlgorithm &other);
+  RandomWalkAlgorithm &operator=(const RandomWalkAlgorithm &other);
+
+  /// find path from start to finish
+  /// \return list of connected coordinates
+  /// if returned list is empty the path does not exist
+  std::vector<Coord> FindPath();
+
+  /// find path from start to finish
+  /// \return list of connected coordinates
+  /// if returned list is empty the path does not exist
+  std::vector<Coord> FindPath(Window &window_handle, const ColorScheme &color_scheme);
+
+  ~RandomWalkAlgorithm() {
+    ClearGraph();
+  }
+
+ private:
+  bool GenerateGraph();
+  bool GenerateGraph(Window &window_handle, const ColorScheme &color_scheme);
+
+  void GeneratePath();
+  void GeneratePath(Window &window_handle, const ColorScheme &color_scheme);
+
+  /// \param position center of returned "square"
+  /// \return neighbouring cells to cell under specified position
+  std::vector<Coord> GenNeighbours(const Coord &position);
+
+  Coord PopRandomCell(std::vector<Coord>& positions);
+
+  bool SearchBreakingPoint(const std::vector<Coord>& possible_routs, bool& path_has_been_found);
+
+
+  a_star::Cell &GetCell(const Coord &position) { return copy_plane_[position.ToInt(width_)]; };
+
+  void ClearGraph();
+
+
+   protected:
+    /// x axis
+    unsigned width_;
+    /// y axis
+    unsigned height_;
+
+    std::vector<a_star::Cell> copy_plane_;
+
+    std::vector<Coord> starting_points_;
+    std::vector<Coord> final_points_;
+    std::vector<Coord> shortest_path_;
+
+  };
+
+#endif//COA_RANDOM_WALK_RANDOW_WALK_ALGORITHM_H_
---
+++ random_walk/random_walk_example.cpp
@@ -0,0 +1,31 @@
+//
+// Created by piotr on 09/10/2021.
+//
+#include "../plane/plane.h"
+#include "../sample_algorithm/sample.h"
+#include "random_walk_algorithm.h"
+int main() {
+
+  Plane sic(20, 20);
+
+  sic.AddBorder();
+  sic.SetCell({1, 1}, CellState::FINISH);
+
+  sic.SetCell({18, 18}, CellState::START);
+
+  printf("original maze:\n");
+
+  ConsoleDisplay(sic);
+
+  RandomWalkAlgorithm sample(sic);
+
+  printf("\nsolution:\n");
+
+  ConsoleDisplay(sic,sample.FindPath());
+
+  ConsoleDisplay(sample.FindPath());
+
+  getchar();
+
+  return 0;
+}
