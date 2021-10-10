0c0e31525be6496203f901da41ccc2f7c988c8ec
Piotr Drabik <piotr.drabik@edu.uni.lodz.pl>
2021-10-10 18:06:11 +0200

    message me please

--- CoA_main.cpp
+++ CoA_main.cpp
@@ -12,7 +12,10 @@ int Loop(Window &window) {
   window.MainLoop();
   return 0;
 }
-
+void MessageMe(int maze_nr, std::string maze_type, std::string algorithm, size_t time, int path_length) {
+  std::cout << "maze nr: " << maze_nr << "\tmaze type: " << maze_type << "\talgorithm: " << algorithm << "\ttime:" << time << "us\t"
+            << "path length: " << path_length << "\n";
+}
 int GenBruteForceVisuals(Window &window, ColorScheme color_scheme) {
 
   for (int i = 0; i < 10; ++i) {
@@ -27,14 +30,11 @@ int GenBruteForceVisuals(Window &window, ColorScheme color_scheme) {
 
     auto t1 = std::chrono::steady_clock::now();
     cos.FindPath();
-    printf("maze nr: %d\tmaze type: random\t algorithm: BruteForce\t time: %lld ns\tpath length: %d\n", i, std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
-
-    while (window.GetQueueSize() > 30)
-      std::this_thread::sleep_for(std::chrono::seconds(1));
+    MessageMe(i, "random", "Brute Force", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
   }
   return 420;
 }
-int GenAStarVisuals(Window &window, ColorScheme color_scheme) {
+int GenDijkstraVisuals(Window &window, ColorScheme color_scheme) {
 
   for (int i = 0; i < 10; ++i) {
     Plane sic(100, 100, 10);
@@ -45,12 +45,10 @@ int GenAStarVisuals(Window &window, ColorScheme color_scheme) {
     dijkstra::Dijkstra cos(sic);
 
     auto path = cos.FindPath(window, color_scheme);
+
     auto t1 = std::chrono::steady_clock::now();
     cos.FindPath();
-    printf("maze nr: %d\tmaze type: random\t algorithm: Dijkstra\t time: %lld ns\tpath length: %d\n", i, std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
-
-    while (window.GetQueueSize() > 30)
-      std::this_thread::sleep_for(std::chrono::seconds(1));
+    MessageMe(i, "random", "Dijkstra", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
   }
   return 420;
 }
@@ -68,10 +66,7 @@ int GenRandomWalkVisuals(Window &window, ColorScheme color_scheme) {
 
     auto t1 = std::chrono::steady_clock::now();
     cos.FindPath();
-    printf("maze nr: %d\tmaze type: random\t algorithm: Random Walk\t time: %lld ns\tpath length: %d\n", i, std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
-
-    while (window.GetQueueSize() > 30)
-      std::this_thread::sleep_for(std::chrono::seconds(1));
+    MessageMe(i, "random", "Random Walk", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
   }
   return 420;
 }
@@ -87,14 +82,11 @@ int GenBruteForceVisualsMaze(Window &window, ColorScheme color_scheme) {
 
     auto t1 = std::chrono::steady_clock::now();
     cos.FindPath();
-    printf("maze nr: %d\tmaze type: square maze\t algorithm: BruteForce\t time: %lld ns\tpath length: %d\n", i, std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
-
-    while (window.GetQueueSize() > 30)
-      std::this_thread::sleep_for(std::chrono::seconds(1));
+    MessageMe(i, "square maze", "Brute force", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
   }
   return 420;
 }
-int GenAStarVisualsMaze(Window &window, ColorScheme color_scheme) {
+int GenDijkstraVisualsMaze(Window &window, ColorScheme color_scheme) {
 
   for (int i = 0; i < 10; ++i) {
     MazeGenerator sic(100, 100);
@@ -105,10 +97,7 @@ int GenAStarVisualsMaze(Window &window, ColorScheme color_scheme) {
     auto path = cos.FindPath(window, color_scheme);
     auto t1 = std::chrono::steady_clock::now();
     cos.FindPath();
-    printf("maze nr: %d\tmaze type: square maze\t algorithm: Dijkstra\t time: %lld ns\tpath length: %d\n", i, std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
-
-    while (window.GetQueueSize() > 30)
-      std::this_thread::sleep_for(std::chrono::seconds(1));
+    MessageMe(i, "square maze", "Dijkstra", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
   }
   return 420;
 }
@@ -124,63 +113,57 @@ int GenRandomWalkVisualsMaze(Window &window, ColorScheme color_scheme) {
 
     auto t1 = std::chrono::steady_clock::now();
     cos.FindPath();
-    printf("maze nr: %d\tmaze type: square maze\t algorithm: Random Walk\t time: %lld ns\tpath length: %d\n", i, std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
-
-    while (window.GetQueueSize() > 30)
-      std::this_thread::sleep_for(std::chrono::seconds(1));
+    MessageMe(i, "square maze", "Random Walk", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
   }
   return 420;
 }
-int main() {
 
+int main() {
 
   ColorScheme color_scheme;
   color_scheme.LoadGreenSet();
 
-  Window screen_1(WINDOW_SIZE, WINDOW_SIZE);
+    Window screen_1(Coord(0,0),WINDOW_SIZE, WINDOW_SIZE);
 
-  Window screen_2(Coord(WINDOW_SIZE, 0), WINDOW_SIZE, WINDOW_SIZE);
+    Window screen_2(Coord(WINDOW_SIZE, 0), WINDOW_SIZE, WINDOW_SIZE);
 
-  Window screen_3(Coord(WINDOW_SIZE*2, 0), WINDOW_SIZE, WINDOW_SIZE);
+    Window screen_3(Coord(WINDOW_SIZE*2, 0), WINDOW_SIZE, WINDOW_SIZE);
 
-  Window screen_4(Coord(0, WINDOW_SIZE),WINDOW_SIZE, WINDOW_SIZE);
+//  Window screen_4(Coord(0, WINDOW_SIZE), WINDOW_SIZE, WINDOW_SIZE);
+//
+//  Window screen_5(Coord(WINDOW_SIZE, WINDOW_SIZE), WINDOW_SIZE, WINDOW_SIZE);
+//
+//  Window screen_6(Coord(WINDOW_SIZE * 2, WINDOW_SIZE), WINDOW_SIZE, WINDOW_SIZE);
 
-  Window screen_5(Coord(WINDOW_SIZE, WINDOW_SIZE), WINDOW_SIZE, WINDOW_SIZE);
+    std::thread window_1(Loop, std::ref(screen_1));
+    std::thread window_2(Loop, std::ref(screen_2));
+    std::thread window_3(Loop, std::ref(screen_3));
+//  std::thread window_4(Loop, std::ref(screen_4));
+//  std::thread window_5(Loop, std::ref(screen_5));
+//  std::thread window_6(Loop, std::ref(screen_6));
 
-  Window screen_6(Coord(WINDOW_SIZE*2, WINDOW_SIZE), WINDOW_SIZE, WINDOW_SIZE);
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
+    std::thread generator_1(GenRandomWalkVisuals, std::ref(screen_1), color_scheme);
+    std::thread generator_2(GenBruteForceVisuals, std::ref(screen_2), color_scheme);
+    std::thread generator_3(GenDijkstraVisuals, std::ref(screen_3), color_scheme);
+//  std::thread generator_4(GenRandomWalkVisualsMaze, std::ref(screen_4), color_scheme);
+//  std::thread generator_5(GenBruteForceVisualsMaze, std::ref(screen_5), color_scheme);
+//  std::thread generator_6(GenDijkstraVisualsMaze, std::ref(screen_6), color_scheme);
 
-  std::thread window_thread_1(Loop, std::ref(screen_1));
-  std::thread window_thread_2(Loop, std::ref(screen_2));
-  std::thread window_thread_3(Loop, std::ref(screen_3));
-  std::thread window_thread_4(Loop, std::ref(screen_4));
-  std::thread window_thread_5(Loop, std::ref(screen_5));
-  std::thread window_thread_6(Loop, std::ref(screen_6));
+    generator_1.join();
+    generator_2.join();
+    generator_3.join();
+//  generator_4.join();
+//  generator_5.join();
+//  generator_6.join();
 
-
-  std::this_thread::sleep_for(std::chrono::milliseconds (500));
-
-  std::thread generator_1(GenRandomWalkVisuals, std::ref(screen_1), color_scheme);
-  std::thread generator_2(GenBruteForceVisuals, std::ref(screen_2), color_scheme);
-  std::thread generator_3(GenAStarVisuals, std::ref(screen_3), color_scheme);
-
-  std::thread generator_4(GenRandomWalkVisualsMaze, std::ref(screen_4), color_scheme);
-  std::thread generator_5(GenBruteForceVisualsMaze, std::ref(screen_5), color_scheme);
-  std::thread generator_6(GenAStarVisualsMaze, std::ref(screen_6), color_scheme);
-
-  generator_1.join();
-  generator_2.join();
-  generator_3.join();
-  generator_4.join();
-  generator_5.join();
-  generator_6.join();
-
-  window_thread_1.join();
-  window_thread_2.join();
-  window_thread_3.join();
-  window_thread_4.join();
-  window_thread_5.join();
-  window_thread_6.join();
+    window_1.join();
+    window_2.join();
+    window_3.join();
+//  window_4.join();
+//  window_5.join();
+//  window_6.join();
 
   return 0;
 }
