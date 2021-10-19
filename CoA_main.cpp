f34e5f1983ed537a9c0d7a6e3721652b85cf8067
Piotr Drabik <piotr.drabik@edu.uni.lodz.pl>
2021-10-19 15:45:00 +0200

    main renovation, it's finally good

--- CoA_main.cpp
+++ CoA_main.cpp
@@ -13,225 +13,170 @@
 #define USE_RANDOM_GEN true
 #define USE_SQUARE_MAZE false
 #define USE_CIRCULAR_MAZE false
+enum class MazeType {
+  EMPTY_PLANE,
+  PLANE_5,
+  PLANE_10,
+  PLANE_20,
+  PLANE_25,
+  CIRCUlAR_MAZE,
+  SQUARE_MAZE,
+  SNAIL_MAZE
+};
+enum class Algorythm {
+  DIJKSTRA,
+  A_STAR,
+  RANDOM_WALK
+};
 
-void MessageMe(int maze_nr, const std::string &maze_type, const std::string &algorithm, size_t time, int path_length) {
+void MessageMe(int maze_nr, std::pair<MazeType, Algorythm> settings, size_t time, int path_length) {
+
+  std::string maze_type;
+  std::string algorithm;
+  switch (settings.first) {
+    case MazeType::EMPTY_PLANE: maze_type = "empty plane"; break;
+    case MazeType::PLANE_5:
+      maze_type = "infill 5%";
+      break;
+    case MazeType::PLANE_10:
+      maze_type = "infill 10%";
+      break;
+    case MazeType::PLANE_20:
+      maze_type = "infill 20%";
+      break;
+    case MazeType::PLANE_25:
+      maze_type = "infill 25%";
+      break;
+    case MazeType::CIRCUlAR_MAZE:
+      maze_type = "circular maze";
+      break;
+    case MazeType::SQUARE_MAZE:
+      maze_type = "square maze";
+      break;
+    case MazeType::SNAIL_MAZE:
+      maze_type = "snail maze";
+      break;
+  }
+
+  switch (settings.second) {
+    case Algorythm::DIJKSTRA:
+      algorithm = "Dijkstra";
+      break;
+    case Algorythm::A_STAR: algorithm = "A*"; break;
+    case Algorythm::RANDOM_WALK: algorithm = "Random Walk"; break;
+  }
   std::cout << "maze nr: " << maze_nr << "\tmaze type: " << maze_type << "\talgorithm: " << algorithm << "\ttime:" << time << "us\t"
             << "path length: " << path_length << "\n";
 }
-int GenDijkstraVisuals(Window &window, ColorScheme color_scheme) {
 
-  for (int i = 0; i < 10; ++i) {
-    Plane sic(100, 100, 10);
+#define T_START std::chrono::high_resolution_clock::now()
+#define T_RECORD(t_1) std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - (t_1)).count()
 
-    sic.SetCell({0, 0}, CellState::FINISH);
-    sic.SetCell({50, 50}, CellState::START);
+class Generator {
+ public:
+  Generator(std::pair<MazeType, Algorythm> settings, Window &window, const ColorScheme &color_scheme) {
 
-    Dijkstra cos(sic);
+    Window screen(WINDOW_SIZE, WINDOW_SIZE);
 
-    auto path = cos.FindPath(window, color_scheme);
+    generator_thread_ = new std::thread(&Generator::MainLoop, this, settings, std::ref(screen), color_scheme);
+  };
 
-    auto t1 = std::chrono::steady_clock::now();
-    cos.FindPath();
-    MessageMe(i, "random", "Brute Force", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
+  void MainLoop(std::pair<MazeType, Algorythm> settings, Window &window, ColorScheme color_scheme) {
+    std::chrono::steady_clock::time_point t_1;
+    double time = 0;
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    for (int i = 0; i < 10; ++i) {
+      MazeGenerator maze(100, 100);
+      switch (settings.first) {
+        case MazeType::EMPTY_PLANE:
+          maze.GenRandomMaze(0);
+          break;
+        case MazeType::PLANE_5:
+          maze.GenRandomMaze(5);
+          break;
+        case MazeType::PLANE_10:
+          maze.GenRandomMaze(10);
+          break;
+        case MazeType::PLANE_20:
+          maze.GenRandomMaze(20);
+          break;
+        case MazeType::PLANE_25:
+          maze.GenRandomMaze(25);
+          break;
+        case MazeType::CIRCUlAR_MAZE:
+          maze.GenCircularMaze();
+          break;
+        case MazeType::SQUARE_MAZE:
+          maze.GenSquareMaze();
+          break;
+        case MazeType::SNAIL_MAZE:
+          maze.GenSnailMaze();
+          break;
+      }
+
+      switch (settings.second) {
+        case Algorythm::DIJKSTRA: {
+          Dijkstra dijstra(maze.GetPlane());
+          auto path = dijstra.FindPath(window, color_scheme);
+          t_1 = T_START;
+          dijstra.FindPath();
+          time = T_RECORD(t_1);
+          MessageMe(i, settings, time, path.size());
+        } break;
+        case Algorythm::A_STAR: {
+          AStar a_star(maze.GetPlane());
+          auto path = a_star.FindPath(window, color_scheme);
+          t_1 = T_START;
+          a_star.FindPath();
+          time = T_RECORD(t_1);
+          MessageMe(i, settings, time, path.size());
+
+        } break;
+        case Algorythm::RANDOM_WALK: {
+          RandomWalk random_walk(maze.GetPlane());
+          auto path = random_walk.FindPath(window, color_scheme);
+          t_1 = T_START;
+          random_walk.FindPath();
+          time = T_RECORD(t_1);
+          MessageMe(i, settings, time, path.size());
+        } break;
+      }
+    }
+  };
+
+  ~Generator() {
+    generator_thread_->join();
+    delete generator_thread_;
   }
-  return 420;
-}
-int GenAStarVisuals(Window &window, ColorScheme color_scheme) {
 
-  for (int i = 0; i < 40; ++i) {
-    Plane sic(100, 100, 10);
+  std::thread *generator_thread_;
+};
 
-    sic.SetCell({0, 0}, CellState::FINISH);
-    sic.SetCell({50, 50}, CellState::START);
+void GlobalVisuals(std::vector<std::pair<MazeType, Algorythm>> settings) {
 
-    AStar cos(sic);
+  std::vector<Window *> windows;
+  windows.reserve(settings.size());
+  std::vector<Generator> generators;
+  generators.reserve(settings.size());
 
-    auto path = cos.FindPath(window, color_scheme);
-
-    auto t1 = std::chrono::steady_clock::now();
-    cos.FindPath();
-    MessageMe(i, "random", "AStar", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
-  }
-  return 420;
-}
-int GenRandomWalkVisuals(Window &window, ColorScheme color_scheme) {
-
-  for (int i = 0; i < 10; ++i) {
-    Plane sic(100, 100, 10);
-
-    sic.SetCell({0, 0}, CellState::FINISH);
-    sic.SetCell({50, 50}, CellState::START);
-
-    RandomWalk cos(sic);
-
-    auto path = cos.FindPath(window, color_scheme);
-
-    auto t1 = std::chrono::steady_clock::now();
-    cos.FindPath();
-    MessageMe(i, "random", "Random Walk", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
-  }
-  return 420;
-}
-
-int GenDijkstraVisualsMaze(Window &window, ColorScheme color_scheme) {
-
-  for (int i = 0; i < 10; ++i) {
-    MazeGenerator sic(100, 100);
-    sic.GenSquareMaze();
-    Dijkstra cos(sic.GetPlane());
-
-    auto path = cos.FindPath(window, color_scheme);
-
-    auto t1 = std::chrono::steady_clock::now();
-    cos.FindPath();
-    MessageMe(i, "square maze", "Brute force", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
-  }
-  return 420;
-}
-int GenAStarVisualsMaze(Window &window, ColorScheme color_scheme) {
-
-  for (int i = 0; i < 10; ++i) {
-    MazeGenerator sic(100, 100);
-    sic.GenSquareMaze();
-
-    AStar cos(sic.GetPlane());
-
-    auto path = cos.FindPath(window, color_scheme);
-    //    std::cout << "queue size: " << window.GetQueueSize();
-    auto t1 = std::chrono::steady_clock::now();
-    cos.FindPath();
-    MessageMe(i, "square maze", "AStar", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
-  }
-  return 420;
-}
-int GenRandomWalkVisualsMaze(Window &window, ColorScheme color_scheme) {
-
-  for (int i = 0; i < 10; ++i) {
-    MazeGenerator sic(100, 100);
-    sic.GenSquareMaze();
-
-    RandomWalk cos(sic.GetPlane());
-
-    auto path = cos.FindPath(window, color_scheme);
-
-    auto t1 = std::chrono::steady_clock::now();
-    cos.FindPath();
-    MessageMe(i, "square maze", "Random Walk", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
-  }
-  return 420;
-}
-
-int GenDijkstraVisualsCMaze(Window &window, ColorScheme color_scheme) {
-
-  for (int i = 0; i < 10; ++i) {
-    MazeGenerator sic(100, 100);
-    sic.GenCircularMaze();
-    Dijkstra cos(sic.GetPlane());
-
-    auto path = cos.FindPath(window, color_scheme);
-
-    auto t1 = std::chrono::steady_clock::now();
-    cos.FindPath();
-    MessageMe(i, "circular maze", "Brute force", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
-  }
-  return 420;
-}
-int GenAStarVisualsCMaze(Window &window, ColorScheme color_scheme) {
-
-  for (int i = 0; i < 10; ++i) {
-    MazeGenerator sic(100, 100);
-    sic.GenCircularMaze();
-
-    AStar cos(sic.GetPlane());
-
-    auto path = cos.FindPath(window, color_scheme);
-    //    std::cout << "queue size: " << window.GetQueueSize();
-    auto t1 = std::chrono::steady_clock::now();
-    cos.FindPath();
-    MessageMe(i, "circular maze", "AStar", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
-  }
-  return 420;
-}
-int GenRandomWalkVisualsCMaze(Window &window, ColorScheme color_scheme) {
-
-  for (int i = 0; i < 10; ++i) {
-    MazeGenerator sic(100, 100);
-    sic.GenCircularMaze();
-
-    RandomWalk cos(sic.GetPlane());
-
-    auto path = cos.FindPath(window, color_scheme);
-
-    auto t1 = std::chrono::steady_clock::now();
-    cos.FindPath();
-    MessageMe(i, "circular maze", "Random Walk", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
-  }
-  return 420;
-}
-int GenDijkstraVisualsSMaze(Window &window, ColorScheme color_scheme) {
-
-  for (int i = 0; i < 10; ++i) {
-    MazeGenerator sic(100, 100);
-    sic.GenSnailMaze();
-    Dijkstra cos(sic.GetPlane());
-
-    auto path = cos.FindPath(window, color_scheme);
-
-    auto t1 = std::chrono::steady_clock::now();
-    cos.FindPath();
-    MessageMe(i, "snail maze", "Brute force", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
-  }
-  return 420;
-}
-int GenAStarVisualsSMaze(Window &window, ColorScheme color_scheme) {
-
-  for (int i = 0; i < 10; ++i) {
-    MazeGenerator sic(100, 100);
-    sic.GenSnailMaze();
-
-    AStar cos(sic.GetPlane());
-
-    auto path = cos.FindPath(window, color_scheme);
-    //    std::cout << "queue size: " << window.GetQueueSize();
-    auto t1 = std::chrono::steady_clock::now();
-    cos.FindPath();
-    MessageMe(i, "snail maze", "AStar", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
-  }
-  return 420;
-}
-int GenRandomWalkVisualsSMaze(Window &window, ColorScheme color_scheme) {
-
-  for (int i = 0; i < 10; ++i) {
-    MazeGenerator sic(100, 100);
-    sic.GenSnailMaze();
-
-    RandomWalk cos(sic.GetPlane());
-
-    auto path = cos.FindPath(window, color_scheme);
-
-    auto t1 = std::chrono::steady_clock::now();
-    cos.FindPath();
-    MessageMe(i, "snail maze", "Random Walk", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - t1).count(), path.size());
-  }
-  return 420;
-}
-int main() {
-  srand(time(NULL));
   ColorScheme color_scheme;
   color_scheme.LoadGreenSet();
 
-  Window screen_1(Coord(0, 0), WINDOW_SIZE, WINDOW_SIZE);
-  std::thread generator_1(GenDijkstraVisualsSMaze, std::ref(screen_1), color_scheme);
+  for (int i = 0; i < settings.size(); ++i) {
 
-  Window screen_2(Coord(WINDOW_SIZE, 0), WINDOW_SIZE, WINDOW_SIZE);
-  std::thread generator_2(GenRandomWalkVisualsSMaze, std::ref(screen_2), color_scheme);
+    windows.push_back(new Window({{i * WINDOW_SIZE, 0}, WINDOW_SIZE, WINDOW_SIZE}));
 
-  Window screen_3(Coord(WINDOW_SIZE * 2, 0), WINDOW_SIZE, WINDOW_SIZE);
-  std::thread generator_3(GenAStarVisualsSMaze, std::ref(screen_3), color_scheme);
+    generators.emplace_back(settings[i], *windows[i], color_scheme);
+  }
+}
+int main() {
+  srand(time(NULL));
 
-  generator_1.join();
-  generator_2.join();
-  generator_3.join();
+  std::vector<std::pair<MazeType, Algorythm>> settings = {{MazeType::SQUARE_MAZE, Algorythm::A_STAR},
+                                                          {MazeType::CIRCUlAR_MAZE, Algorythm::DIJKSTRA},
+                                                          {MazeType::SQUARE_MAZE, Algorythm::A_STAR},
+                                                          {MazeType::CIRCUlAR_MAZE, Algorythm::DIJKSTRA}};
+  GlobalVisuals(settings);
 
   return 0;
 }
